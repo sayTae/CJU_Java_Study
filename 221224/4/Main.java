7eb10bcb364d6cb84d8249fe96f90db3e72c521f
TaeYeong19 <mattam1129@gmail.com>
2022-12-25 00:44:08 +0900

    코드_간략화
    1. 제목 그대로

--- 221224/4/Main.java
+++ 221224/4/Main.java
@@ -5,20 +5,9 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String a = br.readLine();
-
 		StringBuilder sb = new StringBuilder();
 
-		// for(int i=0; i<13; i++) {
-		// 	sb.append(a.charAt(i));
-		// }
-		// System.out.println(sb.toString());
-
-		for(int i=0; i<13; i++) {
-			sb.append(a.charAt(i));
-		}
+		sb.append(br.readLine());
 
 		sb.insert(6, "-");
 
