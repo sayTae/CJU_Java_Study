2775da92cb92ca26dc5ecff133a9f2e2461e96df
TaeYeong19 <mattam1129@gmail.com>
2023-01-07 22:07:57 +0900

    수정해서 업로드
    1. 제목 그대로.

--- 230103/2/Main.java
+++ 230103/2/Main.java
@@ -6,13 +6,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st;
 
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 
-		st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
