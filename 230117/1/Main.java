2b4b1b60e335b474c1874de553b364bbe5f6d9d3
TaeYeong19 <mattam1129@gmail.com>
2023-01-18 01:41:29 +0900

    수정
    1. 제목 그대로.

--- 230117/1/Main.java
+++ 230117/1/Main.java
@@ -1,16 +1,20 @@
 
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println(factorial(5));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+
+		System.out.println(factorial(N));
 	}
 
 	static int factorial(int n) {
 		switch(n) {
-			case 0:
-				return 1;
-			default:
-				return n * factorial(n-1);
+			case 0 : { return 1; }
+			default : { return n * factorial(n-1); }
 		}
 	}
 }
--- 230117/3/Main.java
+++ 230117/3/Main.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 		System.out.printf("철수 : ");
